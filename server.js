50706c03227c2f8040a8000dc65750673701499f
Mantina Sagar <123088264+SagarMantina@users.noreply.github.com>
2025-01-18 22:04:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://rapid-keys-4fglw2ho4-mantina-sagars-projects.vercel.app",
     credentials: true,
   })
 );
